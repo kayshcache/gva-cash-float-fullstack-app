2a4303ddffc3e94ac980912da8bd78bf331b20c1
Kaysh <29114402+kayshcache@users.noreply.github.com>
2020-03-13 05:15:52 +1100

    Update app.js

--- express-api/app.js
+++ express-api/app.js
@@ -1,25 +1,38 @@
+// Third party packages
 import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
+// Modules from this repository
 import indexRouter from './routes/indexRouter';
-import mainRouter from './routes/mainRouter';
+import thingsRouter from './routes/thingsRouter';
+
+// This is bespoke class created for this stack
 import MongoAtlas from './database';
+
 
 const app = express();
 
-const dbName = 'things'
-const database = new MongoAtlas(dbName);
+// Set up database
+const databaseName = 'things'
+const database = new MongoAtlas(databaseName);
 database.connect();
 
+// Set up middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Use bespoke routers - as many as you like
 app.use('/', indexRouter);
-app.use('/main', mainRouter);
+app.use('/main', thingRouter);
 
+// Must keep the non-ES6 syntax for now
 module.exports = app;
+
+// The app is launched from a different file you will find here: ./bin/www
+// This is the way Express-Generator does it. Alternatively you can put the 
+// listening code here.
