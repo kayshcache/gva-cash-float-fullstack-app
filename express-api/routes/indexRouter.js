f1322dc1d9599fde2bff71dac1e51d677058ad20
thcl_hkr <kayshcache@github.com>
2020-03-13 06:35:31 +1100

    Minor fix

--- express-api/routes/indexRouter.js
+++ express-api/routes/indexRouter.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET index.html from the public directory
+ * 
+ * Could be used for a docs page for the API
+ *
+ * */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Docs' });
 });
 
 module.exports = router;
