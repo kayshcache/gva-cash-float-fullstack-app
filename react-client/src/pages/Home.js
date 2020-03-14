23f959ac39c0abe253664095cd1d42730ca3e746
thcl_hkr <kayshcache@github.com>
2020-03-14 11:13:23 +1100

    Added Axios and get request on Home.js & setState

--- react-client/src/pages/Home.js
+++ react-client/src/pages/Home.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-function Home() {
-  return (
-    <div className="Home">
-      <h1>Home page</h1>
-      <Link to="/about">Go to About page</Link>
-    </div>
-  )
+export default class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  conponentDidMount() {
+    // Through the proxy (set in package.json)
+    axios.get('/things')
+      .then(res => {
+	// handle success
+	this.setState(res.data);
+	console.log(res);
+      })
+      .catch(err => {
+	// handle error
+	console.warn(err);
+      })
+      .finally(() => {
+	// always executed
+      });
+  }
+
+  render() {
+    return (
+      <div className="Home">
+	<h1>Home page</h1>
+	<Link to="/about">Go to About page</Link>
+      </div>
+    )
+  }
 }
 
-export default Home;
